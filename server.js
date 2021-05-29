e3e7e429d251a199994ee2a81f81371c79fb8647
Lefayy <jadendeleij@gmail.com>
2021-05-29 12:25:39 +0100

    heavy commit

--- server.js
+++ server.js
@@ -4,10 +4,19 @@ const port = 3000;
 
 app.use(express.static('public'));
 app.use(express.static('img'));
+app.use(express.static('javascript'));
 
 // Homepage
 app.get('/', (req, res) => {
     res.sendFile('index.html', { root: __dirname + "/html" });
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile('login.html', { root: __dirname + "/html" });
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile('register.html', { root: __dirname + "/html" });
 })
 
 app.get('/aboutme', (req, res) => {
