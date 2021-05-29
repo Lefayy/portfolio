64ef73763ed09989c4127118c09f3aa1a68dbe0a
Lefayy <jadendeleij@gmail.com>
2021-05-29 11:53:05 +0100

    commit

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
+const port = 3000;
 
 app.use(express.static('public'));
+app.use(express.static('img'));
 
+// Homepage
 app.get('/', (req, res) => {
     res.sendFile('index.html', { root: __dirname + "/html" });
 })
 
-app.listen(3000, () => console.log("Listening on port 3000"));
+app.get('/aboutme', (req, res) => {
+    res.sendFile('about-me.html', { root: __dirname + "/html" });
+})
+
+app.listen(3000, () => console.log(`Listening on port ${port}`));
